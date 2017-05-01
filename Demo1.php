147c79725d99e8daf5520e4e9b8ef73e554bab9d
amitbhardwaj89 <amit@downtownengineers.com>
2017-05-01 14:23:36 +0530

    Initial commit

---
+++ Demo1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siec
+ * Date: 10/13/2015
+ * Time: 2:36 PM
+ */
+class Demo1{
+    var $title,$price;
+    function
+}
---
+++ first.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siec
+ * Date: 9/29/2015
+ * Time: 6:00 PM
+ */
+echo "Learning PHP" . "<br>";
+for ( $i = 0; $i < 5; $i ++ ) {
+	print 'Amit Bhardwaj';
+	print '<br>';
+}
+?>
